f9fb4f0096456f1e166976232a40517bc84d6930
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2024-10-29 03:35:50 +0900

    [level 1] Title: 예산, Time: 1.11 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            if (d[i] <= budget) {
+                budget -= d[i];
+            }
+            else {
+                answer = i;
+                break;
+            }
+            answer = i+1;
+        }
+        return answer;
+    }
+}
