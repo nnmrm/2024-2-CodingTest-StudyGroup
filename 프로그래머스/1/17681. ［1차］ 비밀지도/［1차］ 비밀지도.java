51d86b7bee0a157b96959f7fc2e1099b88a6f12c
Kang Sujin <151008307+nnmrm@users.noreply.github.com>
2024-11-14 02:28:17 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.89 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,24 +1,6 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-
-        String[] answer = new String[n];
-
-         for (int i = 0; i < n; i++) {
-
-             // OR 연산
-             int OrOperation = arr1[i] | arr2[i];
-
-             // 이진수 변환
-             String binaryNum = Integer.toBinaryString(OrOperation);
-
-             // 자릿수 맞추기
-             while (binaryNum.length() < n) { 
-                 binaryNum = "0" + binaryNum;
-             }
-
-             answer[i] = binaryNum.replace('0', ' ').replace('1','#'); //  문자 변환 : 0 -> " ", 1 -> "#"
-
-         }
+        String[] answer = {};
         return answer;
     }
 }
